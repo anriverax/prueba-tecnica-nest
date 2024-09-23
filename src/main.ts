7484645f89ac0b8acdf1f21c64ecd4111c34b5a1
Anibal Rivera <anriverax@gmail.com>
2024-09-23 08:33:32 -0600

    body-parse

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 /* eslint-disable */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.use(bodyParser.json({ limit: '20mb' }));
+  app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
+
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
@@ -15,6 +20,8 @@ async function bootstrap() {
       },
     }),
   );
+  app.enableCors();
+
   await app.listen(3001, () =>
     console.log(`
 🚀 Server ready at: http://localhost:3001`),
