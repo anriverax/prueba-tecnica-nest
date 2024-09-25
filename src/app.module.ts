f9aeb1340efa7b88c9db4a59c2accd9aab693429
Anibal Rivera <anriverax@gmail.com>
2024-09-24 23:26:33 -0600

    update cloudinary

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MulterModule } from '@nestjs/platform-express';
+
 import configuration from './config/configuration';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { validate } from './config/env.validation';
@@ -9,11 +9,6 @@ import { UserModule } from './core/user/user.module';
 
 @Module({
   imports: [
-    MulterModule.register({
-      limits: {
-        fileSize: 20 * 1024 * 1024, // 50 MB en bytes
-      },
-    }),
     ConfigModule.forRoot({
       validate,
       isGlobal: true,
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { json, urlencoded } from 'express';
 
 /* eslint-disable */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true, bodyParser: true });
   app.enableCors();
-  app.use(json({ limit: '20mb' }));
-  app.use(urlencoded({ limit: '20mb', extended: true }));
 
   app.useGlobalPipes(
     new ValidationPipe({
